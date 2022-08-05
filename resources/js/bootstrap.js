b221174b251acf793f0b70834f955b467de831eb
Roman Ashikov <rmn.shkv@gmail.com>
2022-08-05 15:43:41 +0700

    remove bootstrap

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,10 +1,6 @@
 import _ from 'lodash';
 window._ = _;
 
-try {
-    require('bootstrap');
-} catch (e) {}
-
 /**
  * We'll load the axios HTTP library which allows us to easily issue requests
  * to our Laravel back-end. This library automatically handles sending the
@@ -15,23 +11,3 @@ import axios from 'axios';
 window.axios = axios;
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
-
-/**
- * Echo exposes an expressive API for subscribing to channels and listening
- * for events that are broadcast by Laravel. Echo and event broadcasting
- * allows your team to easily build robust real-time web applications.
- */
-
-// import Echo from 'laravel-echo';
-
-// window.Pusher = require('pusher-js');
-
-// window.Echo = new Echo({
-//     broadcaster: 'pusher',
-//     key: process.env.MIX_PUSHER_APP_KEY,
-//     cluster: process.env.MIX_PUSHER_APP_CLUSTER,
-//     forceTLS: true
-// });
-
-import '../sass/app.scss';
-import * as bootstrap from 'bootstrap';
