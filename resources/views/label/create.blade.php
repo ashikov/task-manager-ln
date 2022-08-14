8c5c1ac1ab8787c2f3a36bcd9fdc0e5ea35cf8c1
Roman Ashikov <rmn.shkv@gmail.com>
2022-08-14 18:34:13 +0700

    add input text forms component

--- resources/views/label/create.blade.php
+++ resources/views/label/create.blade.php
@@ -9,12 +9,7 @@
         <div>
             {{ Form::label('name', __('views.label.create.labels.name')) }}
         </div>
-        <div class="mt-2">
-            {{ Form::text('name', null, ['class' => 'rounded border-gray-300 w-1/3']) }}
-        </div>
-        @error('name')
-            <div class="text-rose-600">{{ $message }}</div>
-        @enderror
+        <x-forms.input-name/>
         <div class="mt-2">
             {{ Form::label('description', __('views.label.create.labels.description')) }}
         </div>
--- resources/views/task_status/edit.blade.php
+++ resources/views/task_status/edit.blade.php
@@ -8,12 +8,7 @@
         <div>
             {{ Form::label('name', __('views.task_status.create.labels.name')) }}
         </div>
-        <div class="mt-2">
-            {{ Form::text('name', null, ['class' => 'rounded border-gray-300 w-1/3']) }}
-        </div>
-        @error('name')
-            <div class="text-rose-600">{{ $message }}</div>
-        @enderror
+        <x-forms.input-name/>
         <div class="mt-2">
             {{ Form::submit(__('views.task_status.create.buttons.create'), ['class' => 'bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded']) }}
         </div>
